c3cf4f88515daabdd911ba99da49318110f6d778
TuUsuarioGitHub <cesarivanmorenoacero3@gmail.com>
2026-03-02 08:09:03 -0500

    implementation of a mapping model

---
+++ src/main/java/com/usta/apiproductos/model/Producto.java
@@ -0,0 +1,34 @@
+package com.usta.apiproductos.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+
+    @Column(length = 1000)
+    private String descripcion;
+
+    private String tipo;
+
+    private Double precio;
+
+    private Integer stock;
+
+    private String imagenUrl;
+
+    private Boolean activo;
+}
